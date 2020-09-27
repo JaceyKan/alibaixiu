3d66b402a0b87118dd6bde1194082ebfa4b90623
JaceyKan <jacey_kan@sina.com>
2020-09-27 09:58:57 +0800

    修改服务器端口号为8080

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require(express);
 
 const app = express();
 
-app.listen(3000);
-console.log('服务器创建成功，请访问http://localhost:3000');
+app.listen(8080);
+console.log('服务器创建成功，请访问http://localhost:8080');
