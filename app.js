82bca56323648b0a43b4114e84947509214f643d
JaceyKan <jacey_kan@sina.com>
2020-09-27 10:26:54 +0800

    程序员B添加首页响应

--- app.js
+++ app.js
@@ -2,5 +2,9 @@ const express = require(express);
 
 const app = express();
 
+app.use('/', (req, res) => {
+    res.send('欢迎访问网站首页');
+})
+
 app.listen(8080);
 console.log('服务器创建成功，请访问http://localhost:8080');
